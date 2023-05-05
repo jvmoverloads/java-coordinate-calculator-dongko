16b54317b6d134602bf5eacfcf9708f9905c1d6f
dongKos <dhkim931101@naver.com>
2023-05-05 21:23:56 +0900

    chore: 쓸데없는 실행 코드 제거

--- src/main/java/com/dongko/fuel_injection/FuelInjector.java
+++
@@ -1,6 +0,0 @@
-package com.dongko.fuel_injection;
-
-public class FuelInjector {
-    public void report() {
-    }
-}
--- src/main/java/com/dongko/fuel_injection/Main.java
+++
@@ -1,7 +0,0 @@
-package com.dongko.fuel_injection;
-
-public class Main {
-    public static void main(String[] args) {
-        new FuelInjector().report();
-    }
-}
